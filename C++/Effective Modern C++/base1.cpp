c9b03e267e21947efda7f428caa28f2b6f86d17e
Leon <manki0zz@126.com>
2026-05-14 19:57:37 +0800

    update

---
+++ C++/Effective Modern C++/base1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    const int a = 10;
+    int b = a;
+    
+    const int *const p = new int(10);
+    // int *p1=p;
+    // int *const p2=p;
+    const int *p3 = p;
+    //int *p4 = &a;
+
+    const int &r1 = 20;
+    // int &r2 = a;
+    // int &r3 = r1;
+
+    return 0;
+}
---
+++ C++/Effective Modern C++/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+/*
+
+*/
+
+static int sjhash[26];
+int main()
+{
+     //int hash[26];
+    for (int i = 0; i < 26; i++)
+    {
+        cout << sjhash[i] << endl;
+    }
+    return 0;
+}
+
